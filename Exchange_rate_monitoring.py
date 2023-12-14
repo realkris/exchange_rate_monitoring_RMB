e0ccad39c709ed4ab1d8d1ad95b9330d10290117
realkris <35628554+realkris@users.noreply.github.com>
2023-12-14 18:50:14 -0500

    Update Exchange_rate_monitoring.py

--- Exchange_rate_monitoring.py
+++ Exchange_rate_monitoring.py
@@ -9,17 +9,17 @@ url = "https://srh.bankofchina.com/search/whpj/search_cn.jsp"
 Form_Data = {}
 Form_Data['erectDate'] = ''
 Form_Data['nothing'] = ''
-aus = 500
-bri = 1000
+
 cnt = 0
 while True:
     cnt = cnt+1
     try:
+        from datetime import datetime
+        current_date_and_time = datetime.now()
+        
         Form_Data['pjname'] = '加拿大元'
         data = parse.urlencode(Form_Data).encode('utf-8')
         html = request.urlopen(url,data).read()
-        from datetime import datetime
-        current_date_and_time = datetime.now()
         soup = BeautifulSoup(html,'html.parser')
         div = soup.find('div', attrs = {'class':'BOC_main publish'})
         table = div.find('table')
@@ -27,10 +27,8 @@ while True:
         tr = table.find_all('tr')
         td = tr[1].find_all('td')
         msg = f'{current_date_and_time: }' + 'Canadian Dollar '+td[-1].get_text()+" "+td[3].get_text()
-
         print(msg)
-
-
+        
         Form_Data['pjname'] = '美元'
         data = parse.urlencode(Form_Data).encode('utf-8')
         html = request.urlopen(url,data).read()
@@ -43,9 +41,9 @@ while True:
         
         msg = f'{current_date_and_time: }' + 'US Dollar  '+td[-1].get_text()+" "+td[3].get_text()
         print(msg)
-        time.sleep(60)
+        time.sleep(5)
 
     except Exception as e:
         print(e)
-        time.sleep(30)
+        time.sleep(60)
         continue
